5d9059d55220422b09928a174ae97b2b134d1858
samleemcknight <samleemcknight@gmail.com>
2021-04-07 17:18:30 -0600

    separates style for components with more involved style

---
+++ style/styles.js
@@ -0,0 +1,37 @@
+module.exports = {
+  view: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 10
+  },
+  image: {
+    marginTop: 10,
+    marginBottom: 30,
+    width: "100%",
+    height: 200,
+  },
+  boldText: {
+    fontWeight: 'bold',
+    padding: 20,
+    fontSize: 20,
+  },
+  text: {
+    padding: 20,
+    fontSize: 20,
+  },
+  dietText: {
+    fontSize: 15,
+  },
+  buttonContainer: {
+    width: "100%",
+    justifyContent: "center",
+  },
+  button: {
+    fontSize: 30,
+    color: "white",
+    textAlign: "center"
+  },
+  background: {
+    backgroundColor: "#135762",
+  }
+}
