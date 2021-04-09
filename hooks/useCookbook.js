99d2d118d016a71ee86aca95672280181a3fbb3a
samleemcknight <samleemcknight@gmail.com>
2021-04-09 12:59:04 -0600

    adds Cookbook hook

---
+++ hooks/useCookbook.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react'
+import CookbookModel from '../models/cookbook'
+
+export default function useCookbook(recipeId, recipeTitle) {
+  const [cookbook, setCookbook] = useState([])
+
+  function getCookbook(id, title) {
+    CookbookModel.show(id, title).then(data => {
+      console.log(data)
+      setCookbook(data.recipe)
+    })
+    
+  }
+  useEffect(() => {
+    getCookbook(recipeId, recipeTitle)
+  }, [recipeTitle])
+  
+  return [cookbook, getCookbook]
+}
